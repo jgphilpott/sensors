b23a776944666f2c8a66f4d1818d282f6ac5609c
Jacob Philpott <jacob.gary.philpott@gmail.com>
2021-09-19 20:40:33 -0400

    Waveshare Laser Update

--- Lasers/Waveshare_Laser/laser.py
+++ Lasers/Waveshare_Laser/laser.py
@@ -1,26 +1,25 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+count = 0
+signal = 0
 
-# define data pins on breakout board of Raspberry Pi Model B
-# ... corresponds to 'dout' pin on laser module
 data_input_pin = 18
 
-# define data_input_pin as input to Raspberry Pi
-GPIO.setup(data_input_pin,GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(data_input_pin, GPIO.IN)
 
-icount=0
-icount_signal = 0
-
-while icount_signal < 10:
+while count < 10:
 
     if GPIO.input(data_input_pin):
-        icount += 1
-        input_save=1
+
+        signal = 0
+
     else:
-        if input_save==1:
-            icount_signal += 1
-            print("\n GPIO.input(data_input_pin) = 0!")
-            print(' icount_signal = %s' % icount_signal)
-            input_save=0
-            print('\n -- end --\n')
+
+        if signal == 0:
+
+            count += 1
+            signal = 1
+
+            print("\nSignal detected!")
+            print("Count = %s" % count)
